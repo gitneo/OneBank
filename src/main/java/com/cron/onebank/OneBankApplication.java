5d4ad45afd693d7449cd173ed9ee2bcacbbc0b87
Ogo Okafor <ogookafor@gmail.com>
2022-12-21 10:11:12 +0100

    Initial commit

---
+++ src/main/java/com/cron/onebank/OneBankApplication.java
@@ -0,0 +1,13 @@
+package com.cron.onebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneBankApplication.class, args);
+    }
+
+}
